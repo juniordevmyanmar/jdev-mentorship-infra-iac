1f23968f265981bd0849bf93b950a5ee3afbbcb7
minkyawswar <kyawswar141996@gmail.com>
2023-04-17 18:28:01 +0630

    Feat: Local Variable used for tags

---
+++ locals.tf
@@ -0,0 +1,15 @@
+
+locals {
+  project = "Junior Dev Myanmar Batch-1"
+  env     = "Development"
+
+  tags = {
+    common = {
+
+      Name        = "jdm-bastion-host"
+      Project     = local.project
+      Environment = var.env
+      ManagedBy   = "terraform"
+    }
+  }
+}
