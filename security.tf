eb09084f7e79420f880547661087c610d9a8e586
minkyawswar <kyawswar141996@gmail.com>
2023-05-09 19:57:04 +0630

    Initial

--- security.tf
+++ security.tf
@@ -1,20 +1,14 @@
 
-resource "aws_security_group" "jdm-public-sg" {
-  name        = "jdm-public-sg"
-  description = "Allow SSH Connection from Outside"
+# NAT instance Security Group
+resource "aws_security_group" "nat" {
+  name        = "NAT-security-group"
+  description = "NAT Instance Security group"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -25,15 +19,21 @@ resource "aws_security_group" "jdm-public-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "jdm-public-sg"
-  }
+  tags = local.common_tags
 }
 
-resource "aws_security_group" "jdm-private-sg" {
-  name        = "jdm-private-sg"
-  description = "Allow Only from Public Subnet"
+# Kubernetes Instance Security Group
+resource "aws_security_group" "kubernetes" {
+  name        = "Kubernetes-security-group"
+  description = "Kubernetes Instance Security Group"
   vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
@@ -42,20 +42,13 @@ resource "aws_security_group" "jdm-private-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow All Traffic only from jdm-public-sg
+  # Allow All Traffic only from nat security group
   ingress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    security_groups = [aws_security_group.jdm-public-sg.id]
+    security_groups = [aws_security_group.nat.id]
   }
 
-  tags = {
-    Name = "jdm-private-sg"
-  }
-}
-
-resource "aws_key_pair" "jr-dev-mm" {
-  key_name   = "jr-dev-mm"
-  public_key = file("jr-dev-mm.pub")
+  tags = local.common_tags
 }
